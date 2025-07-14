46844b83b3bce601e93c7b7a4f3b83ae75ed129b
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-07-14 15:57:16 +0100

    feat(SOS-AUTO-API):update data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DriverModule } from './driver/driver.module';
-import { PartnerModule } from './partner/partner.module';
-import { MechanicModule } from './partner/mechanic/mechanic.module';
-import { TowingModule } from './partner/towing/towing.module';
-import { InsurerModule } from './partner/insurer/insurer.module';
-import { AdminModule } from './admin/admin.module';
+import { PartnerModule } from './modules/partner/partner.module';
+import { MechanicModule } from './modules/partner/mechanic/mechanic.module';
+import { TowingModule } from './modules/partner/towing/towing.module';
+import { InsurerModule } from './modules/partner/insurer/insurer.module';
+import { AdminModule } from './modules/admin/admin.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 
 @Module({
